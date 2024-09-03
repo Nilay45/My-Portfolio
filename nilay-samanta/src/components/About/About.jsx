9a7f2acdd3e8f0063f833017f5190951a428ed1a
Nilay45 <nilay.15samanta@gmail.com>
2024-09-04 01:59:02 +0530

    This is my portfolio

---
+++ nilay-samanta/src/components/About/About.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+import styles from "./About.module.css";
+import { getImageUrl } from "../../utils";
+
+export const About = () => {
+  return (
+    <section className={styles.container} id="about">
+      <h2 className={styles.title}>About</h2>
+      <div className={styles.content}>
+        <img
+          src={getImageUrl("about/aboutImage.png")}
+          alt="Me sitting with a laptop"
+          className={styles.aboutImage}
+        />
+          <li className={styles.aboutItem}>
+            <img src={getImageUrl("about/cursorIcon.png")} alt="Cursor icon" />
+            <div className={styles.aboutItemText}>
+              <h3>Frontend Developer</h3>
+              <p>
+              I'm a front-end web developer skilled in HTML, CSS, JavaScript, and ReactJS. I specialize in creating responsive, user-friendly websites with clean, efficient code. My focus is on bringing designs to life and enhancing user experiences.
+              </p>
+            </div>
+          </li>
+      </div>
+    </section>
+  );
+};
---
+++ nilay-samanta/src/components/Contact/Contact.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+import styles from "./Contact.module.css";
+import { getImageUrl } from "../../utils";
+
+export const Contact = () => {
+  return (
+    <footer id="contact" className={styles.container}>
+      <div className={styles.text}>
+        <h2>Contact</h2>
+        <p>Feel free to reach out!</p>
+      </div>
+      <ul className={styles.links}>
+        <li className={styles.link}>
+          <img src={getImageUrl("contact/emailIcon.png")} alt="Email icon" />
+          <a href="mailto:nilay.15samanta@gmail.com">nilay.15samanta@gmail.com</a>
+        </li>
+        <li className={styles.link}>
+          <img
+            src={getImageUrl("contact/linkedinIcon.png")}
+            alt="LinkedIn icon"
+          />
+          <a href="https://www.linkedin.com/in/nilay-samanta-84228b281/">linkedin.com/nilay-samanta</a>
+        </li>
+        <li className={styles.link}>
+          <img src={getImageUrl("contact/githubIcon.png")} alt="Github icon" />
+          <a href="https://github.com/Nilay45">github.com/nilay-samanta</a>
+        </li>
+      </ul>
+    </footer>
+  );
+};
