6aaa76fe3e40f3d4ecf483399677210796468bdf
Nilay45 <nilay.15samanta@gmail.com>
2025-03-10 23:52:06 +0530

    Portfolio updated

--- nilay-samanta/src/components/About/About.jsx
+++ nilay-samanta/src/components/About/About.jsx
@@ -8,17 +8,17 @@ export const About = () => {
     <section className={styles.container} id="about">
       <h2 className={styles.title}>About</h2>
       <div className={styles.content}>
-        <img
+        {/* <img
           src={getImageUrl("about/aboutImage.png")}
           alt="Me sitting with a laptop"
           className={styles.aboutImage}
-        />
+        /> */}
           <li className={styles.aboutItem}>
             <img src={getImageUrl("about/cursorIcon.png")} alt="Cursor icon" />
             <div className={styles.aboutItemText}>
-              <h3>Frontend Developer</h3>
+              <h3>Full-stack Developer</h3>
               <p>
-              I'm a front-end web developer skilled in HTML, CSS, JavaScript, and ReactJS. I specialize in creating responsive, user-friendly websites with clean, efficient code. My focus is on bringing designs to life and enhancing user experiences.
+              I'm a full-stack web developer skilled in HTML, CSS, JavaScript, ReactJS, Node.js, and MongoDB. I specialize in building responsive, user-friendly applications with clean, efficient code, handling both front-end and back-end development to create seamless digital experiences with a robust database.
               </p>
             </div>
           </li>
--- nilay-samanta/src/components/Hero/Hero.jsx
+++ nilay-samanta/src/components/Hero/Hero.jsx
@@ -9,7 +9,7 @@ export const Hero = () => {
       <div className={styles.content}>
         <h1 className={styles.title}>Hi, I'm Nilay Samanta</h1>
         <p className={styles.description}>
-          I'm a front-end developer specializing in React. Reach out if you'd like to learn more!
+        I'm a full-stack developer specializing in React and Node.js. Reach out if you'd like to learn more!
         </p>
         <a href="mailto:nilay.15samanta@gmail.com" className={styles.contactBtn}>
           Contact Me
